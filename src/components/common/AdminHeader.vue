876ed8d34946a5811678e5bee0b6f1cd56664cbd
jiee <jee.vvvv@gmail.com>
2020-12-31 10:15:44 +0900

    관리자모바일 - header 수정

--- src/components/common/AdminHeader.vue
+++ src/components/common/AdminHeader.vue
@@ -11,44 +11,52 @@
 				</p>
 			</h1>
 			<h2 class="page__title">관리자센터</h2>
-			<nav class="admin-gnb">
-				<ul class="lst-gnb">
-					<li class="lst-gnb__title">
-						<p>부서/사용자</p>
-					</li>
-					<li class="lst-gnb__item">
-						<router-link to="/admin/deptRegistration" class="lst-gnb__link">부서 등록</router-link>
-					</li>
-					<li class="lst-gnb__item">
-						<router-link to="/admin/userRegistration" class="lst-gnb__link">사용자 등록</router-link>
-					</li>
-					<li class="lst-gnb__title">
-						<p>권한</p>
-					</li>
-					<li class="lst-gnb__item">
-						<router-link to="/admin/permissionsGroup" class="lst-gnb__link">권한그룹 등록</router-link>
-					</li>
-					<li class="lst-gnb__item">
-						<router-link to="/admin/permissions" class="lst-gnb__link">권한 등록</router-link>
-					</li>
-					<li class="lst-gnb__title">
-						<p>휴일</p>
-					</li>
-					<li class="lst-gnb__item">
-						<router-link to="/admin/holiday" class="lst-gnb__link">휴일 등록</router-link>
-					</li>
-				</ul>
-			</nav>
-		</div>
-		<div class="buttons">
-			<button class="button__main" @click="goUserView">
-				<span class="icon icon-user"></span>
-				사용자 화면
+			<button type="button" class="button__nav">
+				<span class="nav-bar"></span>
+				<span class="nav-bar"></span>
+				<span class="nav-bar"></span>
+				<span class="blind">메뉴열기</span>
 			</button>
-			<button class="button__logout" @click="logoutUser">
-				<span class="icon icon-logout"></span>
-				로그아웃
-			</button>
+			<div class="gnb-wrap">
+				<nav class="admin-gnb">
+					<ul class="lst-gnb">
+						<li class="lst-gnb__title">
+							<p>부서/사용자</p>
+						</li>
+						<li class="lst-gnb__item">
+							<router-link to="/admin/deptRegistration" class="lst-gnb__link">부서 등록</router-link>
+						</li>
+						<li class="lst-gnb__item">
+							<router-link to="/admin/userRegistration" class="lst-gnb__link">사용자 등록</router-link>
+						</li>
+						<li class="lst-gnb__title">
+							<p>권한</p>
+						</li>
+						<li class="lst-gnb__item">
+							<router-link to="/admin/permissionsGroup" class="lst-gnb__link">권한그룹 등록</router-link>
+						</li>
+						<li class="lst-gnb__item">
+							<router-link to="/admin/permissions" class="lst-gnb__link">권한 등록</router-link>
+						</li>
+						<li class="lst-gnb__title">
+							<p>휴일</p>
+						</li>
+						<li class="lst-gnb__item">
+							<router-link to="/admin/holiday" class="lst-gnb__link">휴일 등록</router-link>
+						</li>
+					</ul>
+				</nav>
+				<div class="buttons">
+					<button class="button__main" @click="goUserView">
+						<span class="icon icon-user"></span>
+						사용자 화면
+					</button>
+					<button class="button__logout" @click="logoutUser">
+						<span class="icon icon-logout"></span>
+						로그아웃
+					</button>
+				</div>
+			</div>
 		</div>
 	</header>
 </template>
