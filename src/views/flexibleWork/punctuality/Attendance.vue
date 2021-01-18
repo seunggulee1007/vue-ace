48d69bb84aef5e75b4be40e6e9048488b7067441
jiee <jee.vvvv@gmail.com>
2021-01-18 18:01:58 +0900

    출퇴근등록 tab수정, 스타일변경

--- src/views/flexibleWork/punctuality/Attendance.vue
+++ src/views/flexibleWork/punctuality/Attendance.vue
@@ -1,5 +1,5 @@
-<template
-	><div class="container user-container">
+<template>
+	<div class="container user-container">
 		<section class="section__contents">
 			<div class="inner-wrap">
 				<div class="section-top">
@@ -92,7 +92,116 @@
 					</div>
 				</div>
 
-				<div class="component-area tab-cnt-wrap">
+				<div class="component-area">
+					<div class="component-box component-box-top">
+						<div class="select-month">
+							<button type="button" class="button__prev">
+								<span class="icon icon-triangle"></span>
+								<span class="blind">이전달</span>
+							</button>
+							<p>2020년 8월</p>
+							<button type="button" class="button__next">
+								<span class="icon icon-triangle"></span>
+								<span class="blind">다음달</span>
+							</button>
+						</div>
+						<div class="lst-working">
+							<button type="button" class="button">근무 계획 변경</button>
+							<ul>
+								<li>
+									<a href="overtime.html">연장 근무 신청</a>
+								</li>
+								<li>
+									<a href="#">휴일 근무 신청</a>
+								</li>
+								<li>
+									<a href="leave.html">휴가 신청</a>
+								</li>
+								<li>
+									<a href="#">외근 신청</a>
+								</li>
+								<li>
+									<a href="business-trip.html">출장 신청</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="tab-area">
+						<el-tabs v-model="activeName" @tab-click="handleClick">
+							<el-tab-pane label="리스트" name="first">
+								<div class="tab-cnt">
+									<div class="table-wrap table--horizontal">
+										<table class="table table__detail">
+											<thead>
+												<tr>
+													<th>일자</th>
+													<th>유연 근무 유형</th>
+													<th>상태</th>
+													<th>출/퇴근 시간</th>
+													<th>기본 근무 시간</th>
+													<th>연장 근무 시간</th>
+													<th>총 근무 시간</th>
+													<th>연차(시간)</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr class="row">
+													<td>08-03 월</td>
+													<td>시차 출퇴근제</td>
+													<td>
+														<ul class="lst-status lst-status-multiple opt1 opt2">
+															<li class="opt1__txt opt__txt">정상</li>
+															<li class="opt2__txt opt__txt">연차</li>
+															<li class="opt3__txt opt__txt">반차</li>
+															<li class="opt4__txt opt__txt">시간연차</li>
+															<li class="opt5__txt opt__txt">출장</li>
+															<li class="opt6__txt opt__txt">반반차</li>
+														</ul>
+													</td>
+													<td>09:00 ~ 13:00</td>
+													<td>4시간</td>
+													<td></td>
+													<td>4시간</td>
+													<td>0.5시간</td>
+												</tr>
+												<tr class="row">
+													<td>08-04 화</td>
+													<td>시차 출퇴근제</td>
+													<td>
+														<ul class="lst-status lst-status-multiple opt1">
+															<li class="opt1__txt opt__txt">정상</li>
+															<li class="opt2__txt opt__txt">연차</li>
+															<li class="opt3__txt opt__txt">반차</li>
+															<li class="opt4__txt opt__txt">시간연차</li>
+															<li class="opt5__txt opt__txt">출장</li>
+															<li class="opt6__txt opt__txt">반반차</li>
+														</ul>
+													</td>
+													<td>09:00 ~ 20:00</td>
+													<td>8시간</td>
+													<td>2시간</td>
+													<td>8시간</td>
+													<td></td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+								</div>
+							</el-tab-pane>
+							<el-tab-pane label="달력" name="second">
+								<div class="tab-cnt">
+									<FullCalendar :options="calendarOptions1" class="calendar" />
+								</div>
+							</el-tab-pane>
+							<el-tab-pane label="그래프" name="third">
+								<div class="tab-cnt">
+									chart
+								</div>
+							</el-tab-pane>
+						</el-tabs>
+					</div>
+				</div>
+				<!-- <div class="component-area tab-cnt-wrap">
 					<div class="tab-cnt show">
 						<div class="component-box component-box-top">
 							<div class="select-month">
@@ -220,14 +329,32 @@
 							</div>
 						</div>
 					</div>
-				</div>
+				</div> -->
 			</div>
 		</section>
 	</div>
 </template>
 
 <script>
-export default {};
+import FullCalendar from '@fullcalendar/vue';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+export default {
+	components: {
+		FullCalendar,
+	},
+	data() {
+		return {
+			activeName: 'first',
+			value1: '',
+			value2: '',
+			calendarOptions1: {
+				plugins: [dayGridPlugin, interactionPlugin],
+				initialView: 'dayGridMonth',
+			},
+		}
+	}
+};
 </script>
 
 <style></style>
