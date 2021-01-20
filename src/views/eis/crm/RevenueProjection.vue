c27de05b54735d2990de4ee1b5c9cb9a5a21ef06
leeseunggu <leesg107@naver.com>
2021-01-20 16:51:31 +0900

    차트 반영

--- src/views/eis/crm/RevenueProjection.vue
+++ src/views/eis/crm/RevenueProjection.vue
@@ -65,7 +65,7 @@
 				</div>
 				<div class="component-area">
 					<strong class="content__title">월별 예상 매출</strong>
-					<div class="chart-wrap"></div>
+					<div class="chart-wrap" ref="chartdiv"></div>
 				</div>
 				<div class="component-area">
 					<strong class="content__title">담당자별 영업 기회</strong>
@@ -124,7 +124,94 @@
 </template>
 
 <script>
-export default {};
+import * as am4core from '@amcharts/amcharts4/core';
+import * as am4charts from '@amcharts/amcharts4/charts';
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+// Themes begin
+am4core.useTheme(am4themes_animated);
+// Themes end
+export default {
+	mounted() {
+		this.chart = am4core.create(this.$refs.chartdiv, am4charts.XYChart);
+		this.chart.scrollbarX = new am4core.Scrollbar();
+		this.chart.data = [
+			{
+				country: '2019년10월',
+				visits: 6.7,
+			},
+			{
+				country: '2019년11월',
+				visits: 17.3,
+			},
+			{
+				country: '2019년12월',
+				visits: 14.5,
+			},
+			{
+				country: '2020년1월',
+				visits: 10.5,
+			},
+			{
+				country: '2020년2월',
+				visits: 7.5,
+			},
+			{
+				country: '2020년3월',
+				visits: 9.3,
+			},
+		];
+
+		// Create axes
+		let categoryAxis = this.chart.xAxes.push(new am4charts.CategoryAxis());
+		categoryAxis.dataFields.category = 'country';
+		categoryAxis.renderer.grid.template.location = 0;
+		categoryAxis.renderer.minGridDistance = 30;
+		categoryAxis.renderer.labels.template.horizontalCenter = 'right';
+		categoryAxis.renderer.labels.template.verticalCenter = 'middle';
+		// categoryAxis.renderer.labels.template.rotation = 270;
+		categoryAxis.tooltip.disabled = true;
+		categoryAxis.renderer.minHeight = 110;
+
+		let valueAxis = this.chart.yAxes.push(new am4charts.ValueAxis());
+		valueAxis.renderer.minWidth = 50;
+
+		// Create series
+		let series = this.chart.series.push(new am4charts.ColumnSeries());
+		series.sequencedInterpolation = true;
+		series.dataFields.valueY = 'visits';
+		series.dataFields.categoryX = 'country';
+		series.tooltipText = '[{categoryX}: bold]{valueY}[/]';
+		series.columns.template.strokeWidth = 0;
+
+		series.tooltip.pointerOrientation = 'vertical';
+
+		series.columns.template.column.cornerRadiusTopLeft = 10;
+		series.columns.template.column.cornerRadiusTopRight = 10;
+		series.columns.template.column.fillOpacity = 0.8;
+
+		// on hover, make corner radiuses bigger
+		let hoverState = series.columns.template.column.states.create('hover');
+		hoverState.properties.cornerRadiusTopLeft = 0;
+		hoverState.properties.cornerRadiusTopRight = 0;
+		hoverState.properties.fillOpacity = 1;
+
+		series.columns.template.adapter.add('fill', (fill, target) => {
+			return this.chart.colors.getIndex(target.dataItem.index);
+		});
+
+		// Cursor
+		this.chart.cursor = new am4charts.XYCursor();
+	},
+	data() {
+		return {
+			chart: Object,
+		};
+	},
+};
 </script>
 
-<style></style>
+<style scoped>
+.chart-wrap {
+	height: 300px;
+}
+</style>
